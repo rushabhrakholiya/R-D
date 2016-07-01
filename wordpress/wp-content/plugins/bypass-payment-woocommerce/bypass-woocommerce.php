92da19a67f14ec3cb7a0e5bf6f75024d4bdb9502
rushabh.rakholiya@elayersinteractive.com <rushabh.rakholiya@elayersinteractive.com>
2016-07-01 18:57:31 +0530

    Change

--- wordpress/wp-content/plugins/bypass-payment-woocommerce/bypass-woocommerce.php
+++ wordpress/wp-content/plugins/bypass-payment-woocommerce/bypass-woocommerce.php
@@ -58,21 +58,53 @@ function isPackageProducts_bypass($productid)
 
 function custom_process_order($order_id) {
 
-    if(is_user_logged_in()){
-        $currentUserID = get_current_user_id();
-        $order = new WC_Order( $order_id );
-        $items = $order->get_items();
-        foreach ( $items as $item ) {            
-            $product_id = $item['product_id'];
-            if(isPackageProducts_bypass($product_id)):
-                $downloadlimit = get_field('download_limit',$product_id);   
-                $productterm = get_field('product_category',$product_id);
-                update_field('user_purchase_limit', $downloadlimit , 'user_'.$currentUserID);
-                update_field('product_term_id', $productterm , 'user_'.$currentUserID);
-            endif;
-        }        
-    }    
+    $order = new WC_Order( $order_id );
+    $customer = get_userdata($order->customer_user);
+    $currentUserID = $customer->ID;
+    $items = $order->get_items();
+    foreach ( $items as $item ) {                      
+        $product_id = $item['product_id'];
+        if(isPackageProducts_bypass($product_id)):
+            $downloadlimit = get_field('download_limit',$product_id);   
+            $productterm = get_field('product_category',$product_id);
+            update_field('user_purchase_limit', $downloadlimit , 'user_'.$currentUserID);
+            update_field('product_term_id', $productterm , 'user_'.$currentUserID);
+        endif;
+    }
     return $order_id;
 }
 /* end code on order complete hook */
+
+// Code goes in theme functions.php or a custom plugin 
+add_filter( 'pre_option_woocommerce_enable_guest_checkout', 'conditional_guest_checkout_based_on_product' );
+function conditional_guest_checkout_based_on_product( $value ) {
+  $restrict_ids = array(); // Replace with product ids which cannot use guest checkout
+
+  $products = get_posts(array(
+      'post_type' => 'product',
+      'numberposts' => -1,
+      'tax_query' => array(
+        array(
+          'taxonomy' => 'product_cat',
+          'field' => 'id',
+          'terms' => get_field('package_category_global','option'), // Where term_id of Term 1 is "1".
+          'include_children' => false
+        )
+      )
+  ));
+  foreach($products as $product) 
+  {
+     $restrict_ids[] = $product->ID;
+  }  
+  if ( WC()->cart ) {
+    $cart = WC()->cart->get_cart();
+    foreach ( $cart as $item ) {
+      if ( in_array( $item['product_id'], $restrict_ids ) ) {
+        $value = "no";
+        break;
+      }
+    }
+  }  
+  return $value;
+}
 ?>
